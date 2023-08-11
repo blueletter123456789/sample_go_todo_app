c4bdba5b67f93f437f010b6b5700d9737819314c
Shuhei Abe <blueletter123456789@gmail.com>
2023-08-11 16:25:01 +0900

    change test code.

--- server_test.go
+++ server_test.go
@@ -12,10 +12,9 @@ import (
 )
 
 func TestServerRun(t *testing.T) {
-
 	l, err := net.Listen("tcp", "localhost:0")
 	if err != nil {
-		t.Fatalf("failed to listen port: %v", err)
+		t.Fatalf("failed to listen port %v", err)
 	}
 	ctx, cancel := context.WithCancel(context.Background())
 	eg, ctx := errgroup.WithContext(ctx)
@@ -27,27 +26,27 @@ func TestServerRun(t *testing.T) {
 		s := NewServer(l, mux)
 		return s.Run(ctx)
 	})
-
 	in := "message"
 	url := fmt.Sprintf("http://%s/%s", l.Addr().String(), in)
+
 	t.Logf("try request to %q", url)
 	rsp, err := http.Get(url)
 	if err != nil {
 		t.Errorf("failed to get: %+v", err)
 	}
-
 	defer rsp.Body.Close()
 	got, err := io.ReadAll(rsp.Body)
 	if err != nil {
-		t.Errorf("failed to read body: %+v", err)
+		t.Fatalf("failed to read body: %v", err)
 	}
 
 	want := fmt.Sprintf("Hello, %s!", in)
 	if string(got) != want {
-		t.Errorf("want: %q, but got %q", want, got)
+		t.Errorf("want %q, but got %q", want, got)
 	}
 
 	cancel()
+
 	if err := eg.Wait(); err != nil {
 		t.Fatal(err)
 	}
