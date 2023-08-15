5ec880c12f844ba3ded5c61cc8bac2c511b5bdf5
Shuhei Abe <blueletter123456789@gmail.com>
2023-08-15 20:22:05 +0900

    modify add and get list method routing.

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require golang.org/x/sync v0.3.0
 require (
 	github.com/caarlos0/env/v6 v6.10.1 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.2 // indirect
+	github.com/go-chi/chi/v5 v5.0.10 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.15.0 // indirect
--- mux.go
+++ mux.go
@@ -1,12 +1,26 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/blueletter123456789/sample_go_todo_app/handler"
+	"github.com/blueletter123456789/sample_go_todo_app/store"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-playground/validator/v10"
+)
 
 func NewMux() http.Handler {
-	mux := http.NewServeMux()
+	mux := chi.NewRouter()
 	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application-json; cahrset=utf-8")
 		_, _ = w.Write([]byte(`{"status": "ok"}`))
 	})
+
+	v := validator.New()
+	at := &handler.AddTask{Store: store.Tasks, Validator: v}
+	mux.Post("/tasks", at.ServeHTTP)
+	lt := &handler.ListTask{Store: store.Tasks}
+	mux.Get("/tasks", lt.ServeHTTP)
+
 	return mux
 }
